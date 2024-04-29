75d8c8ff87abc359ffc02738f39bdc9561545150
D00KZ <christopheraadookie@gmail.com>
2024-04-29 11:58:38 -0400

    V3

--- demos/Calculator/add.js
+++ demos/Calculator/add.js
@@ -6,11 +6,13 @@ function add() {
   var result = numberOne + numberTwo;
   console.log(result);
 }
-
-// add()
+// add();
 
 function init() {
-  console.log("Hello");
+  var addButton = document.getElementById("addButton"); //Line is calling a Id from the index.html page and turning it into a var
+  addButton.onclick = add; //Commanding when the clikc on the "addButton" var to run the add() function (the funtion on top)
 }
 
-init()
+// init();
+window.onload = init; //Saying when window loads to call the function "init()"
+init(); //Running the function "init()" in the output when you run code in code runner
